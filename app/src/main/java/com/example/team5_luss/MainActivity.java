2b61203042a2e6c054101404e8f6356d286c2cae
Shashank <mishrashashank21@gmail.com>
2020-08-21 14:51:18 +0800

    Incomplete code [Connecting to api/App bar/Collection point activity]

--- app/src/main/java/com/example/team5_luss/MainActivity.java
+++ app/src/main/java/com/example/team5_luss/MainActivity.java
@@ -1,15 +1,108 @@
 package com.example.team5_luss;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.stream.Stream;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void trustAllCertificates() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+                            return myTrustedAnchors;
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar =  (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
         System.out.println("test");
+
+        Button btn = findViewById(R.id.CollectionList);
+        if (btn != null){
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    //URL url = null;
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                    try {
+                        String target = "https://10.0.2.2:44312/CollectionPoint";
+                        //String target = "https://www.google.com";
+
+                        trustAllCertificates();
+                        URL url = new URL(target);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                        conn.setRequestMethod("GET");
+                        conn.connect();
+                        InputStream in = url.openStream();
+                        BufferedInputStream bufferedInputStream = new BufferedInputStream(in);
+                        String responseString = bufferedInputStream.toString();
+                        System.out.println(responseString);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                        }
+                    }).start();
+                }
+            });
+        }
     }
 }
