4aa0114277d1795bb19f5121c099d829e462bf26
huajiao925 <lyrali142857@outlook.com>
2020-08-24 21:11:05 +0800

    CP update function done(in debug mode)

--- app/src/main/java/com/example/team5_luss/CollectionActivity.java
+++ app/src/main/java/com/example/team5_luss/CollectionActivity.java
@@ -1,5 +1,6 @@
 package com.example.team5_luss;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,16 +9,30 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import Model.CollectionPoint;
+
 public class CollectionActivity extends AppCompatActivity implements CollectionPointList.SingleChoiceListner {
 
-    TextView currentCP;
+    TextView current_cp;
+    String url = "https://10.0.2.2:44312/CollectionPoint"; //set up the API url you want to call
+    String responseString; // result string
+    CollectionPoint collectionPoint = new CollectionPoint();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collection_points);
 
-        currentCP = findViewById(R.id.current_cp_description);
+        current_cp = findViewById(R.id.current_cp);
 
         Button updateCPBtn = findViewById(R.id.updateBtn);
         updateCPBtn.setOnClickListener(new View.OnClickListener() {
@@ -28,11 +43,79 @@ public class CollectionActivity extends AppCompatActivity implements CollectionP
                 collectionPointList.show(getSupportFragmentManager(), "Collection Points List");
             }
         });
+
+        //call the WEB API
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String target = url + "/" + 1;
+                    trustManager.trustAllCertificates();
+                    URL url = new URL(target);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    int responsecode = conn.getResponseCode();
+                    String inline = "";
+                    if(responsecode !=200){
+                        throw new RuntimeException(String.valueOf(responsecode));
+                    }else{
+                        Scanner sc = new Scanner(url.openStream());
+                        while (sc.hasNext()){
+                            inline += sc.nextLine();
+                        }
+                    }
+                    Gson gson = new Gson();
+                    collectionPoint = gson.fromJson(inline,CollectionPoint.class);
+                    current_cp.setText(collectionPoint.Location + " " + collectionPoint.Description);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     @Override
     public void onPositiveButtonClicked(String[] list, int position) {
-        currentCP.setText(list[position]);
+        current_cp.setText(list[position]);
+
+        //save the new cpID to Department
+        //Shared Preferences:
+        final SharedPreferences pref = getSharedPreferences("user_credentials",MODE_PRIVATE);
+        final int deptID = pref.getInt("deptID",0);
+        final int cpID = (position + 1);
+
+        //call the WEB API
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String target = url + "/" + 1 + "/" + cpID;
+                    trustManager.trustAllCertificates();
+                    URL url = new URL(target);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    int responsecode = conn.getResponseCode();
+                    String inline = "";
+                    if(responsecode !=200){
+                        throw new RuntimeException(String.valueOf(responsecode));
+                    }else{
+                        Scanner sc = new Scanner(url.openStream());
+                        while (sc.hasNext()){
+                            inline += sc.nextLine();
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     @Override
