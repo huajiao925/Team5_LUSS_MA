5a7fe49c9653eeadcce07cebf040b5667237d821
ssoe11 <60837279+ssoe11@users.noreply.github.com>
2020-08-25 00:04:38 +0800

    Disbursement request list view

---
+++ app/src/main/java/Model/ViewModel/CustomRequest.java
@@ -0,0 +1,136 @@
+package Model.ViewModel;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.time.LocalDateTime;
+
+import Model.RequestType;
+import Model.Status;
+
+public class CustomRequest {
+
+    @SerializedName("requestID")
+    public int requestID;
+    @SerializedName("requestStatus")
+    public Status.EOrderStatus requestStatus;
+    @SerializedName("requestDate")
+    public String requestDate;
+    @SerializedName("requestBy")
+    public int requestBy;
+    @SerializedName("modifiedBy")
+    public int modifiedBy;
+    @SerializedName("comment")
+    public String comment;
+    @SerializedName("requestType")
+    public Model.RequestType.ERequestType requestType;
+    @SerializedName("parentRequestID")
+    public int parentRequestID;
+    @SerializedName("collectionTime")
+    public String collectionTime;
+    @SerializedName("retrievalID")
+    public int retrievalID;
+    @SerializedName("requestByName")
+    public String requestByName;
+    @SerializedName("modifiedByName")
+    public String modifiedByName;
+
+    public CustomRequest() {
+    }
+
+    public int getRequestID() {
+        return requestID;
+    }
+
+    public void setRequestID(int requestID) {
+        this.requestID = requestID;
+    }
+
+    public Status.EOrderStatus getRequestStatus() {
+        return requestStatus;
+    }
+
+    public void setRequestStatus(Status.EOrderStatus requestStatus) {
+        this.requestStatus = requestStatus;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public int getRequestBy() {
+        return requestBy;
+    }
+
+    public void setRequestBy(int requestBy) {
+        this.requestBy = requestBy;
+    }
+
+    public int getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(int modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public RequestType.ERequestType getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(RequestType.ERequestType requestType) {
+        this.requestType = requestType;
+    }
+
+    public int getParentRequestID() {
+        return parentRequestID;
+    }
+
+    public void setParentRequestID(int parentRequestID) {
+        this.parentRequestID = parentRequestID;
+    }
+
+    public String getCollectionTime() {
+        return collectionTime;
+    }
+
+    public void setCollectionTime(String collectionTime) {
+        this.collectionTime = collectionTime;
+    }
+
+    public int getRetrievalID() {
+        return retrievalID;
+    }
+
+    public void setRetrievalID(int retrievalID) {
+        this.retrievalID = retrievalID;
+    }
+
+    public String getRequestByName() {
+        return requestByName;
+    }
+
+    public void setRequestByName(String requestByName) {
+        this.requestByName = requestByName;
+    }
+
+    public String getModifiedByName() {
+        return modifiedByName;
+    }
+
+    public void setModifiedByName(String modifiedByName) {
+        this.modifiedByName = modifiedByName;
+    }
+}
+
--- app/src/main/java/com/example/team5_luss/DisbursementListAdapter.java
+++ app/src/main/java/com/example/team5_luss/DisbursementListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.team5_luss;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,41 +8,58 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import java.text.SimpleDateFormat;
-import java.util.List;
+import androidx.annotation.Nullable;
 
-import Model.Request;
+import Model.ViewModel.CustomRequest;
 
-public class DisbursementListAdapter extends ArrayAdapter<Request> {
+public class DisbursementListAdapter extends ArrayAdapter<CustomRequest> {
 
-    public DisbursementListAdapter(@NonNull Context context, int resource, List<Request> requests) {
+    private Context context;
+    private LayoutInflater inflater;
+    private CustomRequest[] requests;
+
+    public DisbursementListAdapter(@NonNull Context context, int resource, CustomRequest[] requests) {
         super(context, resource, requests);
+
+        this.context = context;
+        this.requests = requests;
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View v = convertView;
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        DisbursementListAdapter.viewHolder holder;
 
-        if (v == null) {
-            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.disb_req_item, parent, false);
+        if(inflater == null){
+            inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
         }
 
-        Request request = getItem(position);
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.disb_req_item, null);
 
-        if (request != null){
-            TextView rqtId = v.findViewById(R.id.rqtId);
-            TextView rqtDate = v.findViewById(R.id.rqtDate);
-            TextView rqtBy = v.findViewById(R.id.rqtBy);
-
-            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-            String date = formatter.format(request.getRequestDate());
-            rqtId.setText(request.getRequestID());
-            rqtDate.setText(date);
-            rqtBy.setText(request.getRequestBy());
+            holder = new DisbursementListAdapter.viewHolder();
+            holder.requestID = (TextView)convertView.findViewById(R.id.rqtId);
+            holder.requestDate = (TextView)convertView.findViewById(R.id.rqtDate);
+            holder.requestByName = (TextView)convertView.findViewById(R.id.rqtBy);
+            convertView.setTag(holder);
+        }else{
+            holder =(DisbursementListAdapter.viewHolder) convertView.getTag();
         }
 
-        return v;
+        int requestID = (requests[position].getRequestID());
+        String requestDate = CodeSetting.convertDateString(requests[position].getRequestDate());
+        String requestByName = (requests[position].getRequestByName());
+        holder.requestID.setText(String.valueOf(requestID));
+        holder.requestDate.setText(String.valueOf(requestDate));
+        holder.requestByName.setText(String.valueOf(requestByName));
 
+        return convertView;
     }
+
+    static class viewHolder{
+        TextView requestID;
+        TextView requestDate;
+        TextView requestByName;
+    }
+
 }
