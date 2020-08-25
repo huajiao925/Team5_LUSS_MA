78e9d4acc36e9ed0b3542400a171615757513722
Sarah-Wai <waiphupaing2016@gmail.com>
2020-08-25 21:17:52 +0800

    Change for request

--- app/src/main/java/com/example/team5_luss/DelegateActivity.java
+++ app/src/main/java/com/example/team5_luss/DelegateActivity.java
@@ -1,42 +1,31 @@
 package com.example.team5_luss;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.google.gson.Gson;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.xmlpull.v1.XmlPullParserException;
+import com.google.gson.Gson;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
 
-import Model.CollectionPoint;
 import Model.Request;
 
+public class DelegateActivity extends AppCompatActivity {
 
-public class RequestListActivity extends AppCompatActivity {
-
-    String API_URL = "https://10.0.2.2:44312/request/GetRequestMBByStatusByDept/0/1";
+    String API_URL = "https://10.0.2.2:44312/Delegate/";
     String responseString; // result string
     Request[] RequestList = new Request[]{};
     private String webServiceMessage = "Fail";
@@ -44,17 +33,19 @@ public class RequestListActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     List<Request> requestArrList = new ArrayList<Request>();
     Context context;
-
+    int depID=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = this;
         setContentView(R.layout.request_list);
         recyclerView = findViewById(R.id.rvRequests);
-        new GetRequestAsync().execute();
+        SharedPreferences sharedPreferences= getSharedPreferences("user_credentials",MODE_PRIVATE);
+        depID=sharedPreferences.getInt("deptID",0);
+        new GetCurrentDelegateAsync().execute();
     }
 
-    public class GetRequestAsync extends AsyncTask<Void, Void, String> {
+    public class GetCurrentDelegateAsync extends AsyncTask<Void, Void, String> {
 
         @Override
         protected void onPreExecute() {
@@ -91,7 +82,7 @@ public class RequestListActivity extends AppCompatActivity {
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-           webServiceMessage = "Success";
+            webServiceMessage = "Success";
             return webServiceMessage;
 
         }
--- app/src/main/java/com/example/team5_luss/RequestDetailActivity.java
+++ app/src/main/java/com/example/team5_luss/RequestDetailActivity.java
@@ -1,16 +1,52 @@
 package com.example.team5_luss;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class RequestDetailActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Request;
+import Model.RequestDetails;
+
+public class RequestDetailActivity extends AppCompatActivity implements View.OnClickListener {
+    String API_URL ="";
+    String responseString; // result string
+    RequestDetails[] RequestDetail = new RequestDetails[]{};
+    private String webServiceMessage = "Fail";
+    RequestAdapter adapter;
+    RecyclerView recyclerView;
+    List<RequestDetails> requestDetailItemArrList = new ArrayList<RequestDetails>();
 
     TextView reqIDDetail;
     TextView reqDateDetail;
     TextView reqByDetail;
+    TextView reqComment;
+    int requestID;
+    String comment="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +54,103 @@ public class RequestDetailActivity extends AppCompatActivity {
         setContentView(R.layout.request_detail);
 
         reqIDDetail = findViewById(R.id.detail_reqID);
+        requestID=getIntent().getIntExtra("requestID",0);
         reqIDDetail.setText(getIntent().getStringExtra("requestID"));
 
-        reqDateDetail = findViewById(R.id.detail_reqID);
+        reqDateDetail = findViewById(R.id.detail_reqDate);
         reqDateDetail.setText(getIntent().getStringExtra("requestDate"));
 
-        reqByDetail = findViewById(R.id.detail_reqID);
+        reqByDetail = findViewById(R.id.detail_reqBy);
         reqByDetail.setText(getIntent().getStringExtra("requestBy"));
+
+       // ArrayList<RequestDetails> requestDetailList = (ArrayList<RequestDetails>) getIntent().getSerializableExtra("requestDetailList");
+        List<RequestDetails> requestDetailList = new ArrayList<RequestDetails>();
+        requestDetailList = (ArrayList<RequestDetails>)getIntent().getSerializableExtra("requestDetailList");
+
+        Button btn_accept=findViewById(R.id.btnAccept);
+        btn_accept.setOnClickListener(this);
+
+        Button btn_reject=findViewById(R.id.btnReject);
+        btn_reject.setOnClickListener(this);
+
+
     }
 
     @Override
+    public void onClick(View view) {
+        reqComment=findViewById(R.id.editTextComment);
+        comment=reqComment.getText().toString();
+        switch (view.getId()) {
+            case R.id.btnAccept:
+                API_URL="https://10.0.2.2:44312/request/ApproveRequestByDepHeadMB/"+requestID+"/1/"+ comment;
+                new GetRequestDetailAsync().execute();
+                break;
+            case R.id.btnReject:
+                API_URL="https://10.0.2.2:44312/request/ApproveRequestByDepHeadMB/"+requestID+"/0/"+ comment;
+                new GetRequestDetailAsync().execute();
+                break;
+            default:break;
+        }
+    }
+
+    public class GetRequestDetailAsync extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.i("onPreExecute", "onPreExecute");
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            trustManager.trustAllCertificates();
+
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection conn = null;
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+
+                InputStream in = conn.getInputStream();
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(in);
+                StringBuffer response = new StringBuffer();
+                int data=bufferedInputStream.read();
+                while (data!=-1){
+                    char current = (char) data;
+                    response.append(current);
+                    data=bufferedInputStream.read();
+                }
+                responseString = response.toString();
+
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            webServiceMessage = "Success";
+            return webServiceMessage;
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            ShowMessage(webServiceMessage);
+
+        }
+
+    }
+
+    private void ShowMessage(String msg) {
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
     public void onBackPressed() {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, RequestListActivity.class);
         startActivity(intent);
         finish();
     }
