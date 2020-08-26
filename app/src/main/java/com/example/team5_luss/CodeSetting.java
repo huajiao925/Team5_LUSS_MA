53aeb663207ab6a90e39914652199c454ddcec74
ssoe11 <60837279+ssoe11@users.noreply.github.com>
2020-08-27 05:30:15 +0800

    add code setting - for toastmsg validation & qtyCheck

--- app/src/main/java/com/example/team5_luss/CodeSetting.java
+++ app/src/main/java/com/example/team5_luss/CodeSetting.java
@@ -1,5 +1,11 @@
 package com.example.team5_luss;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +25,25 @@ public  class CodeSetting {
          String[] arrOfStr = dateString.split("T", 2);
          return  arrOfStr[0];
 
+    }
+
+    public static void warningToastMsg(Context context, String message){
+        Toast toast=Toast.makeText(context,message,Toast.LENGTH_SHORT);
+        View view =toast.getView();
+        view.setBackgroundColor(Color.parseColor("#ff6633"));
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setTextColor(Color.WHITE);
+        toast.show();
+    }
+
+    public static boolean isQtyNull(ArrayList<Integer> quantity){
+        int sum = 0;
+        for(int i = 0; i< quantity.size(); i++){
+            sum += quantity.get(i);
+        }
+        if(sum > 0){
+            return true;
+        }
+        else return false;
     }
 }
